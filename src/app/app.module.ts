5a2117fd0230b3b4567b9d114f1d1fc33fbf899f
aredelso <aredelso@syr.edu>
2017-02-04 16:59:43 -0800

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+
+import {  AuthGuard } from './auth.service';
+import {  routes } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVu8MySF8zLkgqai5bd89L9eDGCiIznXw",
+  authDomain: "login-auth-ba43c.firebaseapp.com",
+  databaseURL: "https://login-auth-ba43c.firebaseio.com",
+  storageBucket: "login-auth-ba43c.appspot.com",
+  messagingSenderId: "335881348823"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
